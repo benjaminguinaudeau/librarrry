914933b85f3078345a4035ec97967249107515b0
guiguiplot <bguinaudeau@gmail.com>
2018-10-31 18:20:22 +0100

    init

---
+++ librarian/R/scopus_author_publication.R
@@ -0,0 +1,83 @@
+#' scopus_get_publication_by_author
+#' @param data tibble with a column scopus id
+#' @return entries, is a list containing the publications of each scrapped author id
+#' @export
+
+scopus_get_publication_by_author <- function(data){
+
+  author_search_prog <- progressively(author_search, .n = nrow(data))
+
+  entries <- data %>%
+    rename(scopus_id = dc_identifier) %>%
+    mutate(scopus_id = scopus_id %>% str_extract("\\d+")) %>%
+    select(scopus_id) %>%
+    mutate(publication_information = scopus_id %>% map(~{
+      author_search_prog(au_id = .x,
+                           api_key = api_key,
+                           verbose = F, view = "COMPLETE", count = 25,
+                           http = "https://api.elsevier.com/content/search/scopus",
+                           facets = "subjarea(sort=fd)")
+    }), 
+    api_key = api_key, 
+    retrieval_time = Sys.time()) %>%
+    rename(scrapped_ids = scopus_id)
+  return(entries)
+}
+
+
+
+
+# 
+# #' scopus_parse_author_publication_core
+# #' @param entries an entrie : json respone from get_publication_by_author
+# #' @return a tibble containing the detailed information of the entries
+# #' @export
+# 
+# scopus_parse_author_publication_core <- function(entries){
+#   entries %>%
+#     map2_df(.y = 1:length(entries), ~ {
+#       flat_info <- .x %>%
+#         map_lgl( ~ !is.list(.x)) %>%
+#         .x[.] %>%
+#         map(~{
+#           ifelse(is.null(.x), "", .x)
+#         }) %>%
+#         as_tibble_c
+# 
+#       list_info <- .x %>%
+#         map_lgl( ~ is.list(.x)) %>%
+#         .x[.] %>%
+#         map( ~ {
+#           .x %>%
+#             listify %>%
+#             map_df(list_to_tibble)
+#         })
+#       ### trick: break
+#       list_info <- list_info %>%
+#         map( ~ {
+#           .x %>% list %>% tibble()
+#         }) %>%
+#         bind_cols %>%
+#         janitor::clean_names() %>%
+#         set_names(names(list_info))
+# 
+#       out <- bind_cols(flat_info, list_info)
+# 
+#       return(out)
+#     })
+# }
+# 
+# #' scopus_parse_author_publication
+# #' @param data tibble obtained from get_publication_by_author
+# #' @return a tibble containing all information on all publications of each scraped author
+# #' @export
+# 
+# scopus_parse_author_publication <- function(data){
+#   scopus_parse_author_publication_core_pro <- progressively(scopus_parse_author_publication_core, .n = length(data$publication_information))
+# 
+#   data$publication_information %>%
+#     map2_df(.y = data$scopus_id, ~{
+#       .x$entries %>%
+#         scopus_parse_author_publication_core_pro  %>%
+#         mutate(scraped_id = .y)})
+# }
---
+++ librarian/R/scopus_publication.R
@@ -0,0 +1,46 @@
+#' gentry_core
+#' @param .x a tibble with a dc_identifier column indicating the ids to scrape
+#' @return entries, is a list containing the publications of each scrapped author id
+#' @export
+
+entry_core <- function(.x){
+  message(glue::glue("Retrieving entry {.x$dc_identifier} \t\t {.x$id} from {.x$n} \n"))
+  message(glue(" {.x$dc_title}\n "))
+  response <- abstract_retrieval(id = .x$dc_identifier, 
+                                 identifier = "scopus_id", 
+                                 api_key = api_key, 
+                                 verbose = F)
+  
+  if(response$get_statement$status_code == 429){
+    cat(glue("Quota Exceeded \n Stopped at index {.x$batch}"))
+    return(NULL)
+  } else if (response$get_statement$status_code == 200) {
+    cat("Retrieval was successfull\n \n")
+    return(response$content)
+  } else {
+    cat("\n Retrieval failed \n\n")
+    return(NULL)
+  }
+}
+
+#' scopus_get_publication
+#' @param data tibble with a column scopus id
+#' @return entries, is a list containing the publications of each scrapped author id
+#' @export
+
+
+scopus_get_publication <- function(data){
+  
+  entry_core_prog <- progressively(entry_core, .n = nrow(data))
+  
+  entry <- data %>%
+    #slice(1:4) %>%
+    mutate(dc_identifier = dc_identifier %>% str_extract("\\d+")) %>%
+    mutate(n = n()) %>%
+    mutate(id = 1:n()) %>%
+    split(1:nrow(.)) %>%
+    map(entry_core)  %>% 
+    tibble(entries = ., scrapped_ids = data$dc_identifier)
+  
+  return(entry)
+}
